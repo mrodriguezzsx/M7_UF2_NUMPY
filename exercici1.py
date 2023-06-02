497b1cfcd5f619eee43d22005da54500ccdb6232
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2023-06-02 19:09:47 +0200

    Exercici 2 FET

--- exercici1.py
+++ exercici1.py
@@ -3,6 +3,8 @@ import numpy as np
 # Creacio de la matriu 49x49 amb la diagonal demanada
 def diagonal():
     diagonal = np.diag(np.arange(50))
+    print('Exercici 1')
     print(diagonal)
+    print()
     np.save('exercici1.npy',diagonal)
 
